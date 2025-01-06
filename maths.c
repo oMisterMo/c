4a5d63f4e65137ac65691c1b6f6bd751d7cf0413
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-01-06 00:54:47 +0000

    Add pi conversion to maths.c

--- maths.c
+++ maths.c
@@ -2,6 +2,9 @@
 #include <math.h> // malloc, free
 
 int main() {
+    // Pi
+    // float pi = atan2f(1, 1) * 4; // Add -lm to compile
+
     // Doubles
     printf("%lf\n", sqrt(16)); // 4.000000
     printf("%lf\n", floor(22.7)); // 22.000000
