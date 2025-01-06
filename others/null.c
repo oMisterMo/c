4d6aebecea6a8b6f3098bd2926e1502f5f202e17
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-01-06 23:18:01 +0000

    Add additional resource to explain null vs 0 vs \0

---
+++ others/null.c
@@ -0,0 +1,9 @@
+
+/**
+ * null is a value, 0 is a number, and '\0' is a character.
+ * 
+ * https://stackoverflow.com/questions/1296843/what-is-the-difference-between-null-0-and-0
+ */
+int main() {
+    return 0;
+}
