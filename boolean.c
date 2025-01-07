ca517177d643f6cd4be58a636aef44e4aecbc2e8
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-01-07 01:04:49 +0000

    bool values return 0 or 1

--- boolean.c
+++ boolean.c
@@ -2,13 +2,13 @@
 #include <stdbool.h> // bool
 
 /**
- * boolean values are returned as integers
+ * boolean values are returned as integers 1 or 0
  * 
  * Therefore, you must use the %d format specifier to print a boolean value
  */
 int main() {
     bool myBool = false;
 
-    printf("%d\n", myBool);
+    printf("%d\n", myBool); // 1 or 0
     return 0;
 }
