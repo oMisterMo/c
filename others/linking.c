99a5b48ea1d3cc0e981ce4c68ad787f8c3f24684
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-01-09 13:55:52 +0000

    Add file for linking

---
+++ others/linking.c
@@ -0,0 +1,11 @@
+
+/**
+ * Linking is the process of combining multiple object files together to create a single executable file.
+ * 
+ * <file>.c -l[library_name]
+ */
+int main() {
+
+
+    return 0;
+}
