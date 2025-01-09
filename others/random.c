c46d621641c9bc56dda802dbfe76bc96975c34cf
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-01-09 14:11:23 +0000

    seen random number gen

--- others/random.c
+++ others/random.c
@@ -3,9 +3,26 @@
 #include <stdint.h>
 #include <time.h>
 
-// rand() is a function that generates a random number
+#define RANDOM_MAX 250
+
+/**
+ * rand() generates a random number between 0 and RANDOM_MAX
+ */
 int main() {
+
+	// When you run this loop, you will get the same random number every time
 	for (int i = 0; i < 10; i++) {
 		printf("rand is = %d\n", rand());
 	}
+
+	// srand() is a function that seeds the random number generator
+	srand(time(NULL));
+
+	printf("\n");
+
+	// Random value between 0 and 249
+	for (int i = 0; i < 10; i++) {
+		printf("rand is = %d\n", rand() % RANDOM_MAX);
+	}
+
 }
