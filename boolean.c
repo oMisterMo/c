7d6cb87f91f5eb95a8014b972309a74726f9bfe6
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-01-09 13:59:55 +0000

    Update myBool

--- boolean.c
+++ boolean.c
@@ -7,8 +7,16 @@
  * Therefore, you must use the %d format specifier to print a boolean value
  */
 int main() {
+    printf("Boolean values are returned as integers 1 or 0\n");
+    printf("----------------------------------------------\n");
+    printf("myBool = false\n");
     bool myBool = false;
+    printf("myBool: %d\n", myBool); // 1 or 0
 
-    printf("%d\n", myBool); // 1 or 0
+    printf("\n");
+
+    printf("myBool = true\n");
+    myBool = true;
+    printf("myBool: %d\n", myBool); // 1 or 0
     return 0;
 }
