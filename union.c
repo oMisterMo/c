834d60d8f8bb9e7c5dff1b2cb2af7d0b2985772b
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-01-12 14:22:29 +0000

    Add union.c

---
+++ union.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <assert.h>
+
+
+int main(int argc, char *argv[]) {
+
+
+    // Exactyl like structs but only 1 member can have a value
+    // The same memory space is used for all other members
+    typedef union People {
+        // char a;
+        short b;
+        int c;
+        // long d;
+    } person;
+
+    printf("%ld", sizeof(person));
+    printf("\n");
+
+    // How do you use a uion type? I don know yet
+    // person->b = 2;
+    // person->c = 5;
+
+
+    return 0;
+}
