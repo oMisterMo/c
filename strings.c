0b6455abbc6ef7b247bc5942812646616b8bbbe2
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-01-15 00:24:43 +0000

    Implement copy string method

--- strings.c
+++ strings.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void initialize();
-void copy();
+void copy(char a[], char b[]);
+void copy_string();
 
 int main() {
 
     initialize();
-    copy();
+    copy_string();
 
 
     printf("\n");
@@ -22,6 +24,15 @@ void initialize() {
     // Character array (must manually append '\0')
     char str[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
 
+    // Dynamically allocate
+    char *string = malloc(4);
+    string[0] = 'h';
+    string[1] = 'e';
+    string[2] = 'y';
+    string[3] = '\0';
+
+    printf("\n%s\n\n", string);
+
 
     // It is also important that you know that sizeof will always return the memory size (in bytes), and not the actual string length
     printf("alphabet characters: %ld\n", strlen(alphabet));   // 26 characters
@@ -31,21 +42,32 @@ void initialize() {
     printf("%c\n", greetings[1]);
     str[0] = 'J';
     printf("%s\n", str);
+
+
+    free(string);
+}
+
+void copy(char a[], char b[]) {
+    int i = 0;
+    while( (a[i] = b[i]) != '\0' ) {
+        ++i;
+    }
 }
 
 /**
  * Copies the string from src to dest
  * Note that the size of dest should be large enough to store the result of the two strings combined
  */
-void copy() {
+void copy_string() {
     printf("\n---String Copy---\n");
-    char src[6] = "hi", dest[6] = "empty";
+    char src[6] = "hi", dest[6] = "whats";
 
     printf("Initial source string: %s\n", src);
     printf("Initial destination string: %s\n", dest);
 
     // Copy bytes from src to dest
     strcpy(dest, src);
+    // copy(dest, src);
 
     printf("Final copied string: %s\n", dest);
 }
