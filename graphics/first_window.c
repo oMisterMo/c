f1cc35813353b834f5b41be3c5fa9de8f38a5394
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-01-19 10:48:51 +0000

    raylib - First Window

---
+++ graphics/first_window.c
@@ -0,0 +1,32 @@
+#include "raylib.h"
+
+int main() {
+
+
+    InitWindow(800, 400, "First Window");
+
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        // Input
+
+        // Update
+
+        // Collisions
+
+        // Draw
+        BeginDrawing();
+
+        ClearBackground(BLUE);
+        DrawFPS(20, 20);
+
+        EndDrawing();
+    }
+
+
+    CloseWindow();
+
+
+    return 0;
+}
