056656a972a20f81942211ee1440f8aa5a080e76
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-01-30 15:00:02 +0000

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,19 +3,21 @@
 # Create output directory if it doesn't exist
 mkdir -p graphics/out
 
-# Loop through all C files in the current directory
+# Loop through all C files in the graphics directory
 for file in graphics/*.c; do
     # Get the base name of the file (without extension)
     base=$(basename "$file" .c)
 
-
+    # Skip the test file
     if [ "$file" == "graphics/test.c" ]; then
         continue
     fi
+
+    # Print the name of the file being compiled
     echo "Building $file" 
 
     # Compile the C file and place the output in the 'out' directory
-    gcc "$file" -o "graphics/out/$base" -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+    cc "$file" -o "graphics/out/$base" -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 done
 
 echo "Compilation finished! All output files are in the 'out' directory."
