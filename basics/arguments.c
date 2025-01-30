ef6466adfa871db98e28583a943bcf47a8053778
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-01-30 15:57:36 +0000

    Better comments, comment out arguements stuff

--- basics/arguments.c
+++ basics/arguments.c
@@ -4,17 +4,23 @@
 #include <time.h>
 
 /**
- * argc is the number of arguments passed to the program
- * argv is an array of strings containing the arguments
- * argv[0] is the name of the program
+ *
+ * argv[0] is the name of the program e.g. "arguments.c"
  * argv[1] is the first argument
  * argv[2] is the second argument
+ * 
+ * @param int argc is the number of arguments passed to the program
+ * @param char *argv[] is an array of pointers to characters, each element of the array points to a string (character array)
+ * @return int Returns 0 on success
  */
 int main(int argc, char *argv[]) {
 	printf("Number of args: %d\n\n", argc);
 
-	printf("Start address of string: %p\n", argv);
-	printf("First value: %s\n\n", *argv);
+    // Pointer stuff
+    // printf("-----------------------------\n");
+	// printf("Start address of string: %p\n", argv);
+	// printf("First value: %s\n", *argv);
+    // printf("-----------------------------\n\n");
 
     for (int i = 0; i < argc; i++) {
         printf("argument %d: %s\n", i, argv[i]);
