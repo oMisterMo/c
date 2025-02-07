759c2add2ebe94d535983f5806ea3886f1ddb5e1
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-02-07 20:10:26 +0000

    Turn all graphics on color game

--- graphics/presentation.h
+++ graphics/presentation.h
@@ -1,11 +1,11 @@
 // Presentation
-#define isDrawBackground false
-#define isPrarallaxBackground false
-#define isShowCursor false
-#define isDrawTray false
-#define isDrawCard false
-#define isTweenCard false
-#define isAnimateStars false
+#define isDrawBackground true
+#define isPrarallaxBackground true
+#define isShowCursor true
+#define isDrawTray true
+#define isDrawCard true
+#define isTweenCard true
+#define isAnimateStars true
 #define isAudio false
 
 #define isOff false   // global flag to turn all examples off
