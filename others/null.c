9b07da389a7ae0652cf85a5295e6e226338b0b20
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-02-12 01:39:28 +0000

    Add null characters

--- others/null.c
+++ others/null.c
@@ -5,5 +5,13 @@
  * https://stackoverflow.com/questions/1296843/what-is-the-difference-between-null-0-and-0
  */
 int main() {
+
+
+    // The null character
+    printf("'%d' '%c'\n", '\0', '\0');  // '0' ''
+    // The number 0
+    printf("'%d' '%c'\n", '0', '0');    // '48' '0'
+
+
     return 0;
 }
