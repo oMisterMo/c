c6acc5613e39478c4587707f0e52db5324f841e4
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-02-13 21:10:32 +0000

    struct padding example (not complete)

---
+++ others/padding.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+
+    /**
+     * 32-bit processor takes 4 bytes at a time.
+     * 64-bit processor takes 8 bytes at a time.
+     */
+
+     // 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048
+
+    typedef struct {
+        int x;
+        int y;
+    } Vector2;
+
+    typedef struct {
+        int x;
+        int y;
+        int z;
+    } Vector3;
+
+    typedef struct {
+        int age;        // 4
+        char sex;       // 1
+        char name[8];   // 8
+    } Person;
+
+    printf("Hello world!\n");
+
+    Person mo = { 33, 'M', "Mohammed" };
+    printf("%ld\n", sizeof(Person));
+    
+    
+    
+
+    return 0;
+}
