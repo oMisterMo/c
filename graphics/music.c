348647c351c7de24be1929b178768287608dce6a
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-02-22 13:59:48 +0000

    Hmm, https://www.youtube.com/watch?v=Xdbk1Pr5WXU&list=PLpM-Dvs8t0Vak1rrE2NJn8XYEJ5M7-BqT&index=2

--- graphics/music.c
+++ graphics/music.c
@@ -7,19 +7,37 @@
 
 #define ARRAY_LEN(xs) sizeof(xs) / sizeof(xs[0])
 
-uint64_t global_frames[1024] = { 0 };
+uint64_t global_frames[4410] = { 0 };
 size_t global_frames_count = 0;
 
 void callback(void *bufferData, unsigned int frames) {
-    // printf("frames: %u\n", frames);
+    printf("frames: %u\n", frames);
     // printf("ARRAY_LEN(frames) %ld\n", ARRAY_LEN(global_frames));
 
-    if (frames > ARRAY_LEN(global_frames)) {
-        frames = ARRAY_LEN(global_frames);
+    size_t capacity = ARRAY_LEN(global_frames); // 1024
+
+    // Can fit chunk into remaining space...
+    if (frames <= capacity - global_frames_count) {
+        memcpy(global_frames + global_frames_count, bufferData, sizeof(uint64_t) * frames);
+        global_frames_count += frames;
+    } else if (frames <= capacity) {
+        // frames overflow
+        memmove(global_frames, global_frames + sizeof(uint64_t) * frames, sizeof(uint64_t) * (capacity - frames));
+        memcmp(global_frames + (capacity - frames), bufferData, sizeof(uint64_t) * frames);
+    } else {
+        // everything is full
+        // you can't really do stuff
+        memcpy(global_frames, bufferData, sizeof(uint64_t) * capacity);
+        global_frames_count = capacity;
     }
 
-    memcpy(global_frames, bufferData, sizeof(uint64_t) * frames);
-    global_frames_count = frames;
+    // ===========
+    // if (frames > ARRAY_LEN(global_frames)) {
+    //     frames = ARRAY_LEN(global_frames);
+    // }
+
+    // memcpy(global_frames, bufferData, sizeof(uint64_t) * frames);
+    // global_frames_count = frames;
 }
 
 int main(void) {
@@ -102,14 +120,32 @@ int main(void) {
         ClearBackground(BLACK);
 
         // Draw main
-        for (size_t i  = 0; i < global_frames_count; ++i) {
+        int w = GetScreenWidth();
+        int h = GetScreenHeight();
+        float cell_width = (float) w / global_frames_count;
+        printf("global_frames_count: %ld\n", global_frames_count);
 
+        for (size_t i  = 0; i < global_frames_count; ++i) {
+            // Grab 1 channel (L)
+            int32_t sample = *(int32_t*) &global_frames[i];
+
+            printf("cell_w: %f\n", cell_width);
+            float x = i * cell_width;
+            float y = h / 2;
+            float max_height = h * 0.8;
+            if (sample > 0) {
+                float t = (float) sample / INT32_MAX;
+                DrawRectangle(x, y - max_height * t, cell_width, max_height * t, RED);
+            } else {
+                float t = (float) sample / INT32_MIN;
+                DrawRectangle(x, y, cell_width, max_height * t, RED);
+            }
         }
 
         // Draw music rect
-        DrawRectangle(musicPlayer.x, musicPlayer.y, musicPlayer.width, musicPlayer.height, RAYWHITE);
-        DrawRectangle(musicPlayer.x, musicPlayer.y, (int)(timePlayed * musicPlayer.width), musicPlayer.height, MAROON);
-        DrawRectangleLines(musicPlayer.x, musicPlayer.y, musicPlayer.width, musicPlayer.height, GRAY);
+        // DrawRectangle(musicPlayer.x, musicPlayer.y, musicPlayer.width, musicPlayer.height, RAYWHITE);
+        // DrawRectangle(musicPlayer.x, musicPlayer.y, (int)(timePlayed * musicPlayer.width), musicPlayer.height, MAROON);
+        // DrawRectangleLines(musicPlayer.x, musicPlayer.y, musicPlayer.width, musicPlayer.height, GRAY);
 
         // Draw music progress percentage
         DrawText(TextFormat("%.2f", timePlayed * 100), 20, 20, 30, WHITE);
