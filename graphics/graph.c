5ceb65bc615d1e51683f510cf56cc7eff6b52c49
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-02-26 19:01:22 +0000

    Add checked bg, add sample size enum

--- graphics/graph.c
+++ graphics/graph.c
@@ -1,11 +1,59 @@
+/**
+ * Must do:
+ *          1. Modularize methods
+ *          2. Use raygui to modify values easily
+ *              - Color
+ *              - Radius
+ *              - sin(x)
+ *              - Grid (on/off)
+ *              -
+ *              -
+ *          3. Hot reload C
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include "raylib.h"
 #include "raymath.h"
 
 // #define PI atan2f(1, 1) * 4
+enum SAMPLE_SIZE {
+    SAMEPLE_LOW = 256,
+    SAMEPLE_MEDIUM = 512,
+    SAMEPLE_HIGH = 1024,
+    SAMEPLE_HIGHER = 2048,
+    SAMEPLE_HIGHEST = 4096,
+};
 
+Texture2D GenerateCheckedTexture() {
+    // Generate a checked texture by code
+    int width = GetMonitorWidth(1);
+    int height = GetMonitorHeight(1);
+    // Dynamic memory allocation to store pixels data (Color type)
+    const float FADE = 0.2f;
+    Color *pixels = (Color *)malloc(width*height*sizeof(Color));
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            if (((x/32+y/32)/1)%2 == 0) pixels[y*width + x] = Fade(WHITE, FADE);
+            else pixels[y*width + x] = Fade(GRAY, FADE);
+        }
+    }
+    // Load pixels data into an image structure and create texture
+    Image checkedIm = {
+        .data = pixels,             // We can assign pixels directly to data
+        .width = width,
+        .height = height,
+        .format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8,
+        .mipmaps = 1
+    };
 
+    Texture2D checked = LoadTextureFromImage(checkedIm);
+    UnloadImage(checkedIm);
+    return checked;
+}
 
 int main(void) {
 
@@ -17,30 +65,34 @@ int main(void) {
     InitWindow(screenWidth, screenHeight, "Graph");
 
 
-    // vars
+    printf("-------------------\n");
+    printf("Load Textures\n");
+    printf("-------------------\n");
+    Texture2D checked = GenerateCheckedTexture();
 
-    const float RADIUS = 2;
-    size_t samples = 16;
+    printf("-------------------\n");
+    printf("Init Variables\n");
+    printf("-------------------\n");
+    const float RADIUS = 1;
+    size_t samples = 8;
     float in[samples];
-
-    printf("\n=====================\n");
     for (size_t i = 0; i < samples; ++i) {
         float t = (float) i / samples;
+        in[i] = sinf(2 * PI * t);
+
         printf("t = %.2f, ", t);
-        in[i] = sinf(2 * PI * t) ;
     }
-    printf("\n=====================\n\n");
 
-    RenderTexture target = LoadRenderTexture(screenWidth, screenHeight);
-
-    // Vector2 initialMousePosition = {};
-    // float scale = 2.5f;
-
+    // Camera
     Camera2D camera = { 0 };
     camera.target = (Vector2){ 0 };
     camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
     camera.rotation = 0.0f;
     camera.zoom = 2.5f;
+
+    // ========================================
+
+
 
     SetTargetFPS(60);
 
@@ -61,34 +113,38 @@ int main(void) {
         // Camera zoom center of screen
         camera.zoom += ((float)GetMouseWheelMove()*0.4f);
 
-        if (camera.zoom > 6.0f) camera.zoom = 6.0f;
-        else if (camera.zoom < 0.1f) camera.zoom = 0.1f;
+        // if (camera.zoom > 6.0f) camera.zoom = 6.0f;
+        // else if (camera.zoom < 0.1f) camera.zoom = 0.1f;
 
-        // // Zoom based on mouse wheel
-        // float wheel = GetMouseWheelMove();
-        // if (wheel != 0)
-        // {
-        //     // Get the world point that is under the mouse
-        //     Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+        // Zoom based on mouse wheel
+        float wheel = GetMouseWheelMove();
+        if (wheel != 0){
+            // Get the world point that is under the mouse
+            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
 
-        //     // Set the offset to where the mouse is
-        //     camera.offset = GetMousePosition();
+            // Set the offset to where the mouse is
+            camera.offset = GetMousePosition();
 
-        //     // Set the target to match, so that the camera maps the world space point
-        //     // under the cursor to the screen space point under the cursor at any zoom
-        //     camera.target = mouseWorldPos;
+            // Set the target to match, so that the camera maps the world space point
+            // under the cursor to the screen space point under the cursor at any zoom
+            camera.target = mouseWorldPos;
 
-        //     // Zoom increment
-        //     float scaleFactor = 1.0f + (0.25f*fabsf(wheel));
-        //     if (wheel < 0) scaleFactor = 1.0f/scaleFactor;
-        //     camera.zoom = Clamp(camera.zoom*scaleFactor, 0.125f, 64.0f);
-        // }
+            // Zoom increment
+            float scaleFactor = 1.0f + (0.25f*fabsf(wheel));
+            if (wheel < 0) scaleFactor = 1.0f/scaleFactor;
+            camera.zoom = Clamp(camera.zoom*scaleFactor, 1.0f, 64.0f);
+        }
 
         // Camera reset (zoom and rotation)
         if (IsKeyPressed(KEY_R))
         {
             camera.zoom = 2.5f;
             camera.rotation = 0.0f;
+        }
+        if (IsKeyPressed(KEY_F)) {
+            ToggleFullscreen();
+            camera.target.x = 0;
+            camera.target.y = 0;
         }
 
 
@@ -98,6 +154,7 @@ int main(void) {
         // draw
         BeginDrawing();
             ClearBackground(RAYWHITE);
+            DrawTexture(checked, 0, 0, Fade(WHITE, 0.5f));
             BeginMode2D(camera);
                 // Draw y-axis
                 DrawLine(0, -100, 0, 100, BLACK);
@@ -105,15 +162,25 @@ int main(void) {
                 DrawLine(-100, 0, 100, 0, BLACK);
                 // x
                 DrawText("1", 1 * RAD2DEG - MeasureText("1", 5), 0, 5, BLACK);
+                DrawLine(-100, 1 * RAD2DEG, 100, 1 * RAD2DEG, Fade(GRAY, 0.2f));
+                DrawLine(-100, -1 * RAD2DEG, 100, -1 * RAD2DEG, Fade(GRAY, 0.2f));
                 // y
-                DrawText("1", -5, (1 * RAD2DEG - MeasureText("1", 5)) *  1, 5, BLACK);
-                DrawText("-1", -5, (1 * RAD2DEG - MeasureText("1", 5)) * -1, 5, BLACK);
+                DrawText("1",
+                    -5 - MeasureText("1", 5),   // x
+                    (1 * RAD2DEG) * 1 - 3,      // y
+                    5, BLACK);
+                DrawText("-1",
+                    -5 - MeasureText("-1", 5),  // x
+                    (1 * RAD2DEG) * -1 - 3,     // y
+                    5, BLACK);
 
                 // Draw Points
                 for (size_t i = 0; i < samples; ++i) {
                     float t = (float) i / samples;
                     // in[i] = sinf(2 * PI * t) ;
-                    DrawCircle(t * RAD2DEG, 1 - (in[i] * RAD2DEG), RADIUS, RED);
+                    float x = t * RAD2DEG;
+                    float y = (in[i] * RAD2DEG);
+                    DrawCircle(x, -y, RADIUS, RED);
                 }
 
                 // Draw sample lines
@@ -121,7 +188,6 @@ int main(void) {
                     float t = (float) i / samples;
                     DrawLine(t * RAD2DEG, -100, t * RAD2DEG, 100, Fade(GRAY, 0.2f));
                 }
-
             EndMode2D();
         EndDrawing();
         
@@ -131,7 +197,7 @@ int main(void) {
     printf("Destroy\n");
     printf("-------------------\n");
 
-    UnloadRenderTexture(target);
+    UnloadTexture(checked);
     CloseWindow();
 
     return 0;
