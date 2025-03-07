6c3795d0eef769ad38a923518ce51eed59cb180a
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-03-07 12:52:26 +0000

    Add basic timer

--- graphics/intervals.c
+++ graphics/intervals.c
@@ -1,31 +1,110 @@
 #include "raylib.h"
 #include <stdio.h>
+#include <stdbool.h>
+
+// typedef void (*Function)(void *any, int name, Vector2 position);
+typedef void (*Function)();
+
+typedef struct Timer {
+    // Public members
+    int duration;
+    bool repeat;
+    bool autostart;
+    Function callback;
+
+    // Private members, please no edit
+    int _start_time;
+    bool _active;
+} Timer;
+
+void ActivateTimer(Timer *t) {
+    t->_active = true;
+    t->_start_time = GetTime();
+}
+
+void DeactivateTimer(Timer *t) {
+    t->_active = false;
+    t->_start_time = 0;
+    if (t->repeat) {
+        ActivateTimer(t);
+    }
+}
+
+void InitTimer(Timer *t, int duration, bool repeat, bool autostart, Function callback) {
+    t->duration = duration;
+    t->repeat = repeat;
+    t->callback = callback;
+
+    t-> _start_time = 0;
+    t->_active = false;
+
+    if (autostart) {
+        printf("autostart timer...\n");
+        ActivateTimer(t);
+    }
+}
+
+void UpdateTimer(Timer *t) {
+    if (t->_active) {
+        if (GetTime() - t->_start_time >= t->duration) {
+            // Reached the time
+
+            printf("\n\n");
+            printf("start: %d\n", t->_start_time);
+            printf(":) %d seconds\n", t->duration);
+            if (t->callback != NULL) {
+                // RUN THE CALLBACK, I DONT KNOW HOW YET.
+                t->callback();
+            }
+            DeactivateTimer(t);
+        }
+    }
+}
+
+void callback() {
+    printf("HELLO MO: \n");
+}
 
 int main() {
 
+    printf("-------------------\n");
+    printf("Init Window\n");
+    printf("-------------------\n");
     InitWindow(600, 600, "Intervals");
 
 
+    printf("-------------------\n");
+    printf("Init Variables\n");
+    printf("-------------------\n");
+    // ATTEMPT 1
     Color colors[] = { RED, GREEN, BLUE, ORANGE, PINK, BLACK };
     float spawnInterval = 1.0f; // Every 1 second
     float timeLastSpawn = 0.0;
     int index = 0;
 
+    // ATTEMPT 2
+    printf("sizeof(Timer): %ld\n", sizeof(Timer));
+    Timer t = { 0 };
+    InitTimer(&t, 2, true, true, callback);
 
     SetTargetFPS(60);
+    printf("-------------------\n");
+    printf("GAME\n");
+    printf("-------------------\n");
+
     while (!WindowShouldClose()) {
+
+        // ATTEMPT 1 - Only run after elapsed interval time
         double currentTime = GetTime();
-
-        // Only run after elapsed interval time
         if (currentTime - timeLastSpawn > spawnInterval) {
-
             int len = (sizeof(colors) / sizeof(colors[0])) - 1;
             index = GetRandomValue(0, len);
-
-
             // Reset last spawn
             timeLastSpawn = GetTime();
         }
+
+        // ATTEMPT 2
+        UpdateTimer(&t);
 
 
         BeginDrawing();
