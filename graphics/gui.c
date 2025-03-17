f22ec0c7a947979d2e5f76172f663ae5ecb46688
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-03-17 20:48:36 +0000

    Create simple struct

--- graphics/gui.c
+++ graphics/gui.c
@@ -4,6 +4,10 @@
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 
+typedef struct GUI {
+    bool showMessageBox;
+} GUI;
+
 void HandleInput(Rectangle *rect) {
     if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
         if (CheckCollisionPointRec(GetMousePosition(), *rect)){
@@ -13,30 +17,34 @@ void HandleInput(Rectangle *rect) {
         }
     }
 }
+
 void DrawGame(Rectangle rect) {
     ClearBackground(YELLOW);
     DrawRectangleRec(rect, BLACK);
 }
 
-void DrawGUI(bool *showMessageBox) {
+void DrawGUI(GUI *gui) {
     ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
-    if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Show Message")) *showMessageBox = true;
-    if (*showMessageBox) {
+    if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Show Message")) gui->showMessageBox = true;
+    if (gui->showMessageBox) {
         int result = GuiMessageBox((Rectangle){ 85, 70, 250, 100 },
             "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
 
-        if (result >= 0) *showMessageBox = false;
+        if (result >= 0) gui->showMessageBox = false;
     }
 
 }
 
 int main(void) {
 
-
     InitWindow(900, 600, "GUI");
 
     Rectangle rect = { 0, 0, 100, 100 };
-    bool showMessageBox = false;
+
+    // Rectangle, Text title, bool state
+    GUI gui = {
+        .showMessageBox = false
+    };
 
     SetTargetFPS(60);
     while(!WindowShouldClose()) {
@@ -51,7 +59,7 @@ int main(void) {
         BeginDrawing();
 
             DrawGame(rect);
-            DrawGUI(&showMessageBox);
+            DrawGUI(&gui);
             DrawFPS(20, 20);
 
         EndDrawing();
