612a1ca3347828e82402932f61c865e8965848f2
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-03-20 20:56:53 +0000

    static variable storage location example

--- others/static.c
+++ others/static.c
@@ -14,9 +14,22 @@ void add(int a, int b) {
 
 }
 void sub(int a, int b) {
+    // This is stored with the globals
     static int test = 1;
     printf("%p: sub() int test\n", &test);
+
+    // These are on the stack
+    // printf("%p: a\n", &a);
+    // printf("%p: a\n", &b);
 }
+
+void example() {
+    // Global, to this function only, not visible outside
+    static int count = 0; // Retains its value between calls
+    count++;
+    printf("%p %d\n", &count, count);
+}
+
 
 int main(void) {
 
@@ -26,6 +39,11 @@ int main(void) {
     printf("%p: sub()\n", sub);
     printf("%p: static int age\n", &age);
     sub(2, 1);
+
+    printf("\n");
+    example();  // 1
+    example();  // 2
+    example();  // 3
 
 
     return 0;
