29dd2b25e7329ec6c1ea02b597f46c43434bd52f
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-05-16 00:44:15 +0100

    Create ControlPoint structure

--- graphics/bezier.c
+++ graphics/bezier.c
@@ -6,6 +6,11 @@
 #define WIDTH 800
 #define HEIGHT 800
 
+typedef struct ControlPoint {
+    Vector2 pos;
+    bool isDragging;
+} ControlPoint;
+
 int main() {
     InitWindow(WIDTH, HEIGHT, "Bezier");
     SetTargetFPS(60);
@@ -17,8 +22,10 @@ int main() {
         (Vector2) {WIDTH,HEIGHT + offsetY},
     };
 
-    Vector2 cp = { WIDTH/2, HEIGHT/2};
-    bool dragging = false;
+    ControlPoint cp1 = {
+        .pos = { WIDTH/2, HEIGHT/2},
+        .isDragging = false,
+    };
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -27,39 +34,39 @@ int main() {
         // DrawLine(WIDTH/2, 0, WIDTH/2, HEIGHT, BLACK);  // Y-axis
         // DrawLine(0, HEIGHT/2, WIDTH, HEIGHT/2, BLACK);  // X-axis
 
-        if (CheckCollisionPointCircle(GetMousePosition(), cp, 10)) {
+        if (CheckCollisionPointCircle(GetMousePosition(), cp1.pos, 10)) {
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                dragging = true;
+                cp1.isDragging = true;
             }
             if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-                dragging = true;
+                cp1.isDragging = true;
             }
             if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-                dragging = false;
+                cp1.isDragging = false;
             }
         } else {
-            // dragging = false;
+            // cp1.isDragging = false;
         }
 
         // Quick reset position
         if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
-            cp = GetMousePosition();
+            cp1.pos = GetMousePosition();
         }
 
-        if (dragging) {
-            cp = GetMousePosition();
+        if (cp1.isDragging) {
+            cp1.pos = GetMousePosition();
         }
 
 
         DrawLineBezier((Vector2) {0,0}, (Vector2) {WIDTH,HEIGHT}, 10, RED);
         DrawSplineLinear(linear, sizeof(linear) / sizeof(Vector2), 10, BLUE);
         DrawSplineSegmentBezierQuadratic(
-            (Vector2) {0,0}, cp, (Vector2) {WIDTH,HEIGHT}, 10, ORANGE);
+            (Vector2) {0,0}, cp1.pos, (Vector2) {WIDTH,HEIGHT}, 10, ORANGE);
 
-        if (dragging) {
-            DrawCircleV(cp, 10, RED);
+        if (cp1.isDragging) {
+            DrawCircleV(cp1.pos, 10, RED);
         } else {
-            DrawCircleV(cp, 10, BLACK);
+            DrawCircleV(cp1.pos, 10, BLACK);
         }
 
         DrawFPS(20, 20);
