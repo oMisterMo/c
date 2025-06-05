20c8f0c001c5cd2d394c12fef5e36a35d8083cf5
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-06-05 22:24:42 +0100

    Rename cp1 -> controlPoint

--- graphics/bezier.c
+++ graphics/bezier.c
@@ -17,12 +17,17 @@ int main() {
 
     float offsetY = 200.0f;
 
+    // Bunch of random lines
     Vector2 linear[] = {
-        (Vector2) {0,0 + offsetY},
-        (Vector2) {WIDTH,HEIGHT + offsetY},
+        (Vector2) {0, offsetY},
+        (Vector2) {WIDTH/2,HEIGHT},
+        (Vector2) {WIDTH, offsetY},
     };
 
-    ControlPoint cp1 = {
+    // Line start / end position and control point
+    Vector2 start = { 0,0 };
+    Vector2 end = { WIDTH,HEIGHT };
+    ControlPoint controlPoint = {
         .pos = { WIDTH/2, HEIGHT/2},
         .isDragging = false,
     };
@@ -34,40 +39,47 @@ int main() {
         // DrawLine(WIDTH/2, 0, WIDTH/2, HEIGHT, BLACK);  // Y-axis
         // DrawLine(0, HEIGHT/2, WIDTH, HEIGHT/2, BLACK);  // X-axis
 
-        if (CheckCollisionPointCircle(GetMousePosition(), cp1.pos, 10)) {
+        if (CheckCollisionPointCircle(GetMousePosition(), controlPoint.pos, 10)) {
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                cp1.isDragging = true;
+                controlPoint.isDragging = true;
             }
             if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-                cp1.isDragging = true;
+                controlPoint.isDragging = true;
             }
             if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-                cp1.isDragging = false;
+                controlPoint.isDragging = false;
             }
-        } else {
-            // cp1.isDragging = false;
         }
 
         // Quick reset position
         if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
-            cp1.pos = GetMousePosition();
+            controlPoint.pos = GetMousePosition();
         }
 
-        if (cp1.isDragging) {
-            cp1.pos = GetMousePosition();
+        if (controlPoint.isDragging) {
+            controlPoint.pos = GetMousePosition();
         }
 
-
-        DrawLineBezier((Vector2) {0,0}, (Vector2) {WIDTH,HEIGHT}, 10, RED);
-        DrawSplineLinear(linear, sizeof(linear) / sizeof(Vector2), 10, BLUE);
-        DrawSplineSegmentBezierQuadratic(
-            (Vector2) {0,0}, cp1.pos, (Vector2) {WIDTH,HEIGHT}, 10, ORANGE);
-
-        if (cp1.isDragging) {
-            DrawCircleV(cp1.pos, 10, RED);
+        // Draw contol point - including hover color if dragging
+        if (controlPoint.isDragging) {
+            DrawCircleV(controlPoint.pos, 10, RED);
         } else {
-            DrawCircleV(cp1.pos, 10, BLACK);
+            DrawCircleV(controlPoint.pos, 10, BLACK);
         }
+        // DrawCircleV(controlPoint.pos, 10, controlPoint.isDragging ? RED : BLACK);
+
+
+        // Default curve
+        DrawLineBezier((Vector2) {0,0}, (Vector2) {WIDTH,HEIGHT}, 10, RED);
+        // Multiple lines
+        DrawSplineLinear(linear, sizeof(linear) / sizeof(Vector2), 10, BLUE);
+        // Actual bezier
+        DrawSplineSegmentBezierQuadratic(start, controlPoint.pos, end, 10, ORANGE);
+
+        // DrawSplineSegmentBezierQuadratic(Vector2 p1, Vector2 c2, Vector2 p3, float thick, Color color);              // 1 control point
+        // DrawSplineSegmentBezierCubic(Vector2 p1, Vector2 c2, Vector2 c3, Vector2 p4, float thick, Color color);      // 2 control points
+
+
 
         DrawFPS(20, 20);
         EndDrawing();
