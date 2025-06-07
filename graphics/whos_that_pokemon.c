6da915182ba40efb639ea26dee4550d109eec176
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-06-07 21:13:32 +0100

    Load original 151 pokemons

--- graphics/whos_that_pokemon.c
+++ graphics/whos_that_pokemon.c
@@ -9,9 +9,15 @@
 int main(void) {
     InitWindow(WIDTH, HEIGHT, "Lerp");
 
+    int pokemons = 151;
     // Init variables
     Vector2 pos = { WIDTH/2, HEIGHT/2 };
     Texture2D img = LoadTexture("resources/sprites/114.png");
+    Texture2D imgs[pokemons];
+    int index = 0;
+    for (int i = 0; i < pokemons; i++) {
+        imgs[i] = LoadTexture(TextFormat("resources/sprites/emerald/%d.png", i+1));
+    }
     float scale = 2.0f;
     float t = 0.0f;    // 0 -> 1
 
@@ -24,6 +30,10 @@ int main(void) {
         float mouseX = (float)GetMouseX() / WIDTH;
         // printf("%f\n", mouseX);
         // printf("%d\n", GetMouseX());
+        if (IsKeyPressed(KEY_SPACE)) {
+            printf("heill\n");
+            index = GetRandomValue(0, pokemons);
+        }
 
         //update
         // t += 0.001;
@@ -34,14 +44,22 @@ int main(void) {
             // Game
             float scale = Lerp(1, 10, mouseX);
             Vector2 atOriginScaled = Vector2Subtract(pos, (Vector2) {(img.width*scale)/2, (img.height*scale)/2});
-            DrawTextureEx(img, atOriginScaled, 0, scale, ColorLerp(BLACK, WHITE, mouseX / STRENGTH));
-            DrawCircleV(pos, 10, ColorAlpha(RED, 0.1f));
+
+
+            // DrawTextureEx(img, atOriginScaled, 0, scale, ColorLerp(BLACK, WHITE, mouseX / STRENGTH));
+            DrawTextureEx(imgs[index], atOriginScaled, 0, scale, ColorLerp(BLACK, WHITE, mouseX / STRENGTH));
+
+
+            // DrawCircleV(pos, 10, ColorAlpha(RED, 0.1f));
             // UI
             DrawText(TextFormat("%.2f", mouseX), 20, 20, 30, BLACK);
         EndDrawing();
     }
 
     UnloadTexture(img);
+    for (int i = 0; i < pokemons; i++) {
+        UnloadTexture(imgs[i]);
+    }
     CloseWindow();
 
     return 0;
