2c976cd2cc476ad61c8fb046057b419d415a6a98
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-06-28 14:44:03 +0100

    Do not load 151 individual sprites, use spritesheet

--- graphics/whos_that_pokemon.c
+++ graphics/whos_that_pokemon.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
-#include <raylib.h>
-#include <raymath.h>
+#include "raylib.h"
+#include "raymath.h"
 
 #define WIDTH 800
 #define HEIGHT 800
 #define STRENGTH 5.0f   // 1 = Weak, 3+ = Strong
 
 int main(void) {
-    InitWindow(WIDTH, HEIGHT, "Lerp");
+    InitWindow(WIDTH, HEIGHT, "Whos that Pokemon?");
 
     int pokemons = 151;
     // Init variables
-    Vector2 pos = { WIDTH/2, HEIGHT/2 };
+    Vector2 pos = { WIDTH/2.0f, HEIGHT/2.0f };
     Texture2D img = LoadTexture("resources/sprites/114.png");
-    Texture2D imgs[pokemons];
+    Texture2D spritesheet = LoadTexture("resources/sprites/poke.png");
+    // Texture2D imgs[pokemons];
     int index = 0;
-    for (int i = 0; i < pokemons; i++) {
-        imgs[i] = LoadTexture(TextFormat("resources/sprites/emerald/%d.png", i+1));
-    }
+    // for (int i = 0; i < pokemons; i++) {
+    //     imgs[i] = LoadTexture(TextFormat("resources/sprites/emerald/%d.png", i+1));
+    // }
     float scale = 2.0f;
     float t = 0.0f;    // 0 -> 1
+
+    // Get image from spritesheet
+    Rectangle srcRect = {589,761,62,55};    // random poke
+    // Rectangle srcRect = {741,722,35,37};    // 151 = mew
+    // Rectangle srcRect = {308,377,59,56};    // 3 = venasaur
+    // Location on screen with scale (w and h at the end)
+    // Rectangle destRect = {WIDTH/2.0f, HEIGHT/2.0f, srcRect.width, srcRect.height};
 
     SetTargetFPS(60);
     while(!WindowShouldClose()) {
@@ -30,10 +38,10 @@ int main(void) {
         float mouseX = (float)GetMouseX() / WIDTH;
         // printf("%f\n", mouseX);
         // printf("%d\n", GetMouseX());
-        if (IsKeyPressed(KEY_SPACE)) {
-            printf("heill\n");
-            index = GetRandomValue(0, pokemons);
-        }
+        // if (IsKeyPressed(KEY_SPACE)) {
+        //     printf("heill\n");
+        //     index = GetRandomValue(0, pokemons);
+        // }
 
         //update
         // t += 0.001;
@@ -43,12 +51,17 @@ int main(void) {
             ClearBackground((Color) {213,240,234,1});
             // Game
             float scale = Lerp(1, 10, mouseX);
-            Vector2 atOriginScaled = Vector2Subtract(pos, (Vector2) {(img.width*scale)/2, (img.height*scale)/2});
 
-
+            // Old way
+            // Vector2 atOriginScaled = Vector2Subtract(pos, (Vector2) {(img.width*scale)/2.0f, (img.height*scale)/2.0f});
             // DrawTextureEx(img, atOriginScaled, 0, scale, ColorLerp(BLACK, WHITE, mouseX / STRENGTH));
-            DrawTextureEx(imgs[index], atOriginScaled, 0, scale, ColorLerp(BLACK, WHITE, mouseX / STRENGTH));
+            // DrawTextureEx(imgs[index], atOriginScaled, 0, scale, ColorLerp(BLACK, WHITE, mouseX / STRENGTH));
 
+            // New way
+            // DrawTexture(spritesheet, 0, 0, WHITE);
+            Rectangle destRect = {WIDTH/2.0f, HEIGHT/2.0f, srcRect.width * scale, srcRect.height * scale};
+            DrawTexturePro(spritesheet, srcRect, destRect, (Vector2) {destRect.width/2.0f,destRect.height/2.0f}, 0.0f, ColorLerp(BLACK, WHITE, mouseX / STRENGTH));
+            // DrawTextureEx(spritesheet, atOriginScaled, 0, scale, ColorLerp(BLACK, WHITE, mouseX / STRENGTH));
 
             // DrawCircleV(pos, 10, ColorAlpha(RED, 0.1f));
             // UI
@@ -57,9 +70,10 @@ int main(void) {
     }
 
     UnloadTexture(img);
-    for (int i = 0; i < pokemons; i++) {
-        UnloadTexture(imgs[i]);
-    }
+    UnloadTexture(spritesheet);
+    // for (int i = 0; i < pokemons; i++) {
+    //     UnloadTexture(imgs[i]);
+    // }
     CloseWindow();
 
     return 0;
