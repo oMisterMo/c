40a99fa155cb1ca9e9367a28d0a27aba45e28499
Mohammed Ibrahim <mo-a-ibrahim@hotmail.co.uk>
2025-06-28 03:52:19 +0100

    Add goto.c

---
+++ basics/goto.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void) {
+
+
+    // printf("Goto\n");
+    // int x = 1, y = 2, w = 3, h;
+    // int x, y, w, h;
+    // int x, y, w, h = 0;
+    int x = 0, y = 0, w = 0, h = 0;
+
+
+    char *mo = "mo";
+    char *no = "no";
+
+    printf("%d, %d, %d, %d\n", x, y, w, h);
+    // printf("%.*s\n\n", 5, "hello");
+
+    // for (int i = 0; i < 5; ++i) {
+    //     printf("i is %d\n", i);
+
+    //     if (i == 3) {
+    //         goto end;
+    //     }
+    // }
+
+    // end: printf("end is here..\n");
+    // printf("ok mo\n");
+
+    return 0;
+}
+
